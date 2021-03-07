80075702c5bb772babf336d3f7996d7db351263c
realiti4 <onurcetinkol@gmail.com>
2021-03-07 23:17:48 +0300

    fix import error

--- tradingfeatures/__init__.py
+++ tradingfeatures/__init__.py
@@ -5,8 +5,8 @@ from tradingfeatures.bitstamp import bitstamp
 from tradingfeatures.bitmex import bitmex
 from tradingfeatures.google_trends import google_trends
 
-from tradingfeatures.legacy import bitfinexLegacy
-from tradingfeatures.legacy import bitstampLegacy
+# from tradingfeatures.legacy import bitfinexLegacy
+# from tradingfeatures.legacy import bitstampLegacy
 
 from tradingfeatures.main import base
 
