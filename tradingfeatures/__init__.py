d3fdd505c6d058aa9f7a68c5c7fce5b2fcb6ab97
realiti4 <onurcetinkol@gmail.com>
2021-03-09 01:20:46 +0300

    improve imports

--- tradingfeatures/__init__.py
+++ tradingfeatures/__init__.py
@@ -1,15 +1,9 @@
 from tradingfeatures.api_base import apiBase
-
-from tradingfeatures.bitfinex import bitfinex
-from tradingfeatures.bitstamp import bitstamp
-from tradingfeatures.bitmex import bitmex
-from tradingfeatures.bitmex_v2 import bitmex_v2
-from tradingfeatures.okex import okex
+from tradingfeatures.apis import *
 
 from tradingfeatures.google_trends import google_trends
 
-# from tradingfeatures.legacy import bitfinexLegacy
-# from tradingfeatures.legacy import bitstampLegacy
+from tradingfeatures.legacy import *
 
 from tradingfeatures.main import base
 
