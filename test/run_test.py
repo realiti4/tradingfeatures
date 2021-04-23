5dbd0ca96a002f03c01012535ada16f083b4b85b
realiti4 <onurcetinkol@gmail.com>
2021-04-24 01:56:26 +0300

    adding tests

---
+++ test/run_test.py
@@ -0,0 +1,38 @@
+import pytest
+from tradingfeatures import binance, bitmex, bitfinex, bitstamp, Uber
+
+binance = binance()
+bitmex = bitmex()
+bitfinex = bitfinex()
+bitstamp = bitstamp()
+uber = Uber()
+
+    
+def test_get():
+    df_binance = binance.get()
+    df_bitmex = bitmex.get()
+    df_bitfinex = bitfinex.get()
+    df_bitstamp = bitstamp.get()
+
+    assert df_binance.columns.to_list() == ['open', 'high', 'low', 'close', 'volume', 
+                                        'quote_asset_vol', 'number_of_trades', 'taker_base_asset_vol', 
+                                        'taker_quote_asset_vol', 'taker_sell_base_asset_vol']
+    assert df_bitmex.columns.to_list() == ['open', 'high', 'low', 'close', 'trades', 'volume', 'vwap', 
+                                        'lastSize', 'turnover', 'homeNotional', 'foreignNotional']
+    assert df_bitfinex.columns.to_list() == ['open', 'close', 'high', 'low', 'volume']
+    assert df_bitstamp.columns.to_list() == ['high', 'volume', 'low', 'close', 'open']
+
+def test_get_get_hist():
+    df_binance = binance.get(2000)
+    df_bitmex = bitmex.get(2000)
+    df_bitfinex = bitfinex.get(2000)
+    df_bitstamp = bitstamp.get(2000)
+
+def test_interval_get():
+    df_binance = binance.get(interval='1m')
+    df_bitmex = bitmex.get(interval='1m')
+    df_bitfinex = bitfinex.get(interval='1m')
+    df_bitstamp = bitstamp.get(interval='1m')
+
+def test_uber():
+    df_eval = uber.eval_get(2000)
